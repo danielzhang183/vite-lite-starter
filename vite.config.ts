59e0bed9bb40fb53960237072d7908076f3f32ad
danielzhang183 <danielzhang182@gmail.com>
2022-08-13 06:52:49 +0800

    feat: add icon & unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+
+    // https://github.com/antfu/unocss
+    Unocss(),
+  ],
 })
