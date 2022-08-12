a285c3bc7bcd4de5d20c116c3eacf77bf153eac1
danielzhang183 <danielzhang182@gmail.com>
2022-08-13 07:27:35 +0800

    feat: auto generate router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
 
     // https://github.com/antfu/unocss
     Unocss(),
